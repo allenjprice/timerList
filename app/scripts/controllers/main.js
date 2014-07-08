60d07248f3790d455d0fea70c98b6cde54de3a83
allenjprice <allenjprice@gmail.com>
2014-07-07 21:33:00 -0500

    Set up basic todo list functionality

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,32 @@
  */
 angular.module('timerListApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    
+    $scope.item = {
+      name: '',
+      time: '',
+      done: false
+    };
+
+    $scope.list = [];
+
+    $scope.resetItem = function(){
+      $scope.item = {
+        name: '',
+        time: '',
+        done: false
+      };
+    };
+
+    $scope.submitItem = function(){
+      console.log('submitItem hit');
+      $scope.list.push($scope.item);
+      $scope.resetItem();
+      console.log($scope.list);
+    };
+
+    $scope.toggleComplete = function(item){
+      item.done = !item.done;
+    };
+
   });
